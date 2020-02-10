a81e7ba1c8abb54492fde631c214655f0d6a1b6e
bradehrlich6 <60119950+bradehrlich6@users.noreply.github.com>
2020-02-10 15:48:46 -0500

    Add files via upload
    toDo List Final version

---
+++ toDoList/toDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  toDoList
+//
+//  Created by Brad Ehrlich on 2/10/20.
+//  Copyright © 2020 Brad Ehrlich. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
